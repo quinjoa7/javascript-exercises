340bdb49b0379ed555b944820ccfc3703180ed95
quinjoa7 <quinjoa27@gmail.com>
2022-02-23 19:15:20 -0300

    Exercise 07-tempCOnversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let number = (temp - 32) * 5 / 9;
+  return parseFloat(number.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let number = temp * 9 / 5 + 32
+  return parseFloat(number.toFixed(1));
 };
-
+console.log(ftoc(100))
+console.log(ctof(73.2))
 // Do not edit below this line
 module.exports = {
   ftoc,
