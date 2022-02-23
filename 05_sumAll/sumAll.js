dc9697166a42ff8eef4463917ad3738982f84a50
quinjoa7 <quinjoa27@gmail.com>
2022-02-23 16:47:12 -0300

    Exercise 05-sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let total = 0;
+    let first = 0;
+    let second = 0;
 
+    if (firstInt < 0 || secondInt < 0 || typeof(firstInt) != typeof(secondInt)) {
+        return 'ERROR';
+    }
+
+    if (firstInt < secondInt) {
+        first = firstInt;
+        second = secondInt;
+    } else if (secondInt < firstInt) {
+        first = secondInt;
+        second = firstInt;
+    }
+
+    for (let i = first; i <= second; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
