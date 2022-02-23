9015a9a994842d892c6afc9f6b9061903bcfb3ec
quinjoa7 <quinjoa27@gmail.com>
2022-02-23 16:06:27 -0300

    Exercise 04-removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let list = array;
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < list.length; j++) {
+            if (list[j] === toRemove[i]) {
+                list.splice(j,1);
+            }
+        }
+    }
+    return list;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
