6f7ad9cb68d80e89158109be34b598735324b2bb
quinjoa7 <quinjoa27@gmail.com>
2022-02-23 15:26:30 -0300

    Exercise 03-reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringAsList = string.split('');
+    stringAsList.reverse();
+    let text = stringAsList.join('');
+    return text;
 };
+console.log(reverseString('hola'));
 
 // Do not edit below this line
 module.exports = reverseString;
