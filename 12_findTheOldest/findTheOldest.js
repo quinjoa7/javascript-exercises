f04515b9590cb6b2ddd6de7b9186b0f974e72a24
quinjoa7 <quinjoa27@gmail.com>
2022-03-19 15:20:12 -0300

    Exercise 12- FindeTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const list = a;
 
+    list.forEach(person => {
+      if ('yearOfDeath' in person === false) {
+          const actualTime = new Date();
+          person.lived= actualTime.getFullYear() - person.yearOfBirth;
+      } else {
+          person.lived = person.yearOfDeath - person.yearOfBirth;
+      }
+    })
+
+    list.sort((a,b) => a.lived < b.lived ? 1 : -1);
+    
+    return list[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
