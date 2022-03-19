cb02d40c9dcb6e5427d1ee37e922a4edb3c0e1b4
quinjoa7 <quinjoa27@gmail.com>
2022-03-19 14:22:56 -0300

    Exercise 10-Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    const number = Number(a);
+    const serie = [1,1];
+    for (let i = 0; i < a - 2; i++) {
+        serie.push(serie[i] + serie[i + 1]);
+    }
 
+    return number > 0 ? serie[serie.length - 1] : 'OOPS';
 };
 
 // Do not edit below this line
