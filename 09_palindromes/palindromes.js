67b85724081d37350cc1d0505a7b0091d8097c6e
quinjoa7 <quinjoa27@gmail.com>
2022-03-18 23:58:03 -0300

    Exersice 09_Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    const text = a.replace(/[!,. ]/g,"").toLowerCase();
+    const textAsArray = text.split("");
+    const first = textAsArray.splice(0,Math.floor(textAsArray.length / 2));
+    textAsArray.reverse();
 
+    if (first.length < textAsArray.length) {
+        textAsArray.pop();
+    }
+
+    const q = first.toString();
+    const w = textAsArray.toString();
+
+    return q === w;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
